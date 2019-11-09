7c8957a3ab9dfc68136423044e25df27eff9b05d
Nandor Gallo <nandor.gallo@uoit.net>
2019-11-09 16:30:38 -0500

    Implmented Notfications and Added Time

--- notetakr/lib/LecturePage.dart
+++ notetakr/lib/LecturePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'MyNotesPage.dart';
+import 'utils/Notifications.dart';
 
 class LectureList extends StatefulWidget {
   @override
@@ -13,136 +14,218 @@ class _LectureListState extends State<LectureList> {
     'CSCI 4500',
     ' Intro to Computer Science'
   ];
+  DateTime _classDates = DateTime.now();
+  var _notifications = Notifications();
 
   @override
   Widget build(BuildContext context) {
+    _notifications.init();
+    DateTime now = DateTime.now();
+    //Tabs With Items
     return new DefaultTabController(
         length: 4,
         child: Scaffold(
-            appBar: AppBar(
-              title: Text('NoteTakR'),
-              bottom: TabBar(isScrollable: true, tabs: <Widget>[
-                Tab(
-                  text: 'Notes',
-                  icon: Icon(Icons.list),
-                ),
-                Tab(text: 'Locations', icon: Icon(Icons.map)),
-                Tab(text: 'Graphs', icon: Icon(Icons.multiline_chart)),
-                Tab(text: 'Assignments',icon: Icon(Icons.note),),
-              ]),
-            ),
-            body: TabBarView(
-              children: <Widget>[
+          appBar: AppBar(
+            title: Text('NoteTakR'),
+            bottom: TabBar(isScrollable: true, tabs: <Widget>[
+              Tab(
+                text: 'Notes',
+                icon: Icon(Icons.list),
+              ),
+              Tab(text: 'Locations', icon: Icon(Icons.map)),
+              Tab(text: 'Graphs', icon: Icon(Icons.multiline_chart)),
+              Tab(
+                text: 'Assignments',
+                icon: Icon(Icons.note),
+              ),
+            ]),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Tab(
+                //Grid of Classes
+                  child: GridView.count(
+                      primary: true,
+                      crossAxisCount: 4,
+                      children: (lectures
+                          .map((lecture) =>
+                              new LectureWidget(this.context, lecture))
+                          .toList()))
+                  //TODO: Implement a Future Builder Widget to get Data For the Notes
+                  ),
+              Tab(
+                child: Text('Implement Locations Widgets'),
+              ),
+              Tab(
+                child: Text('Implements Graph Page'),
+              ),
+              Tab(
+                //List of Assignments
+                  child: ListView(
+                      primary: true,
 
-                Tab(
-                    child: GridView.count(
-                        primary: true,
-                        crossAxisCount: 4,
-                        children: (lectures
-                            .map((lecture) =>
-                                new LectureWidget(this.context, lecture))
-                            .toList()))
-                    //TODO: Implement a Future Builder Widget to get Data For the Notes
-                    ),
-                Tab(
-                  child: Text('Implement Locations Widgets'),
-                ),
-                Tab(
-                  child: Text('Implements Graph Page'),
-                ),
-                Tab(
-                  child: Text("Implement Assignments"),
+                      children: (lectures
+                          .map((lecture) =>
+                              new LectureWidget(this.context, lecture))
+                          .toList()))
+                  //TODO: Implement a Future Builder Widget to get Data For the Assignments
                   )
-                
-              ],
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add_comment),
+            onPressed: () {
+              //Create a selected index, to change between pages, Different From adding assignments, to adding classes...
+              print("Add Lectures Page");
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    String new_lecture = "";
+                    String new_code = "";
 
-            ),
-            floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.add_comment),
-              onPressed: () 
-              {
-                print("Add Lectures Page"); 
-                showDialog(
-                context: context,  
-                builder: (BuildContext context) {
-                 String new_lecture=""; 
-                 String new_code = "";
-                 
-                 return new Dialog(
-                  
-                  backgroundColor: Colors.cyan,
-                  child: Card(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Padding(
-                        padding: const EdgeInsets.all(4),
-                        child:Text('Add A New Lecture',style:TextStyle(color: Colors.cyan,fontSize: 15))
-                        ),
-                        Padding(
-                        padding:  const EdgeInsets.all(4),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(), 
-                            hintText: 'Lecture Name',
-                          ),
-                          onChanged: (text) {
-                            new_lecture = text;
-                          },
-                        )
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(4),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(), 
-                              hintText: 'Course Code'
-                            ),
-                            onChanged: (code)
-                            {
-                              new_code = code; 
-                            },
-                          ),
-                        ),
-                        ButtonBar(
+                    return new Dialog(
+                        backgroundColor: Colors.cyan,
+                        child: Card(
+                            child: Column(
+                          mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
-                            FlatButton(
-                              child: Text("Add"), 
-                              onPressed: () 
-                              {
-                                //TODO: ADD notes to database 
-                                _AddLecturetoDB(new_lecture,new_code); 
-                                 Navigator.pop(context);                               
-                                                              },
-                                                            ),
-                                                            FlatButton(
-                                                              child: Text("Cancel"), 
-                                                              onPressed: () 
-                                                              {
-                                                                  Navigator.pop(context); 
-                                                              },
-                                                            )
-                                                          ],
-                                                        )
-                                                      ],
-                                                    )
-                                                  )
-                                                );
-                                                }
-                                                );
-                                              
-                                              },
-                                            ),
-                                            )
-                                            
-                                            );
-                                  }
-                                
-                                  void _AddLecturetoDB(String new_lecture,String new_code) {
-                                     print("Add Lecture $new_lecture  and $new_code to  Database");
-                                     //TODO: Insert new Lecture to model 
+                            Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: Text('Add A New Class',
+                                    style: TextStyle(
+                                        color: Colors.cyan, fontSize: 15))),
+                            Padding(
+                                padding: const EdgeInsets.all(4),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Class Name',
+                                  ),
+                                  onChanged: (text) {
+                                    new_lecture = text;
+                                  },
+                                )),
+                            Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Course Code'),
+                                onChanged: (code) {
+                                  new_code = code;
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  RaisedButton(
+                                    child: Text('Course Days'),
+                                    color: Colors.cyan,
+                                    textColor: Colors.black,
+                                    onPressed: () {
+                                      showDatePicker(
+                                        context: context,
+                                        firstDate: now,
+                                        lastDate: DateTime(2100),
+                                        initialDate: now,
+                                      ).then((value) {
+                                        setState(() {
+                                          _classDates = DateTime(
+                                            value.year,
+                                            value.month,
+                                            value.day,
+                                            _classDates.hour,
+                                            _classDates.minute,
+                                            _classDates.second,
+                                          );
+                                        });
+                                      });
+                                    },
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 25.0),
+                                    child: Text(_toDateString(_classDates)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  RaisedButton(
+                                    child: Text('Course Time'),
+                                    color: Colors.cyan,
+                                    textColor: Colors.black,
+                                    onPressed: () {
+                                      showTimePicker(
+                                        context: context,
+                                        initialTime: TimeOfDay(
+                                          hour: now.hour,
+                                          minute: now.minute,
+                                        ),
+                                      ).then((value) {
+                                        setState(() {
+                                          _classDates = DateTime(
+                                            _classDates.year,
+                                            _classDates.month,
+                                            _classDates.day,
+                                            value.hour,
+                                            value.minute,
+                                          );
+                                        });
+                                      });
+                                    },
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 25.0),
+                                    child: Text(_toTimeString(_classDates)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            ButtonBar(
+                              children: <Widget>[
+                                FlatButton(
+                                  child: Text("Add"),
+                                  onPressed: () {
+                                    //TODO: ADD notes to database
+                                    _AddLecturetoDB(new_lecture, new_code, _classDates);
+                                    //Send a notification 
+                                    _notificationLater(_classDates);
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                FlatButton(
+                                  child: Text("Cancel"),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                )
+                              ],
+                            )
+                          ],
+                        )));
+                  });
+            },
+          ),
+        ));
+  }
+  Future<void> _notificationLater(var dateNotify) async {
+    print(dateNotify);
+    await _notifications.sendNotificationLater('CSCI 4100 Assignment', 'Due: Thursday!', dateNotify, 'payload');
+  }
 
-                                  }
+  void _AddLecturetoDB(String new_lecture, String new_code,DateTime date) {
+    print("Add Class $new_lecture  and $new_code to  Database With $date");
+    //TODO: Insert new Lecture to model
+  }
 }
 
 class LectureWidget extends StatelessWidget {
@@ -156,21 +239,20 @@ class LectureWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Implement build
     return new Card(
-      color: Colors.blueGrey,
-      child: InkWell(
-        splashColor: Colors.blue,
-        onTap: () {
-          print('Card tapped.');
-          _navigatetoAddPage(context, this.title);
-        },
-        child: Container(
-          width: 300,
-          height: 100,
-          
-          child: Center(
-            child: Text(this.title),
-          ),
-        )));
+        color: Colors.blueGrey,
+        child: InkWell(
+            splashColor: Colors.blue,
+            onTap: () {
+              print('Card tapped.');
+              _navigatetoAddPage(context, this.title);
+            },
+            child: Container(
+              width: 300,
+              height: 100,
+              child: Center(
+                child: Text(this.title),
+              ),
+            )));
   }
 
   void _navigatetoAddPage(context, String lecture) {
@@ -178,6 +260,22 @@ class LectureWidget extends StatelessWidget {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => MyNotesPage(lecture)));
   }
-
-  
 }
+
+
+
+
+String _twoDigits(int value) {
+    if (value < 10) {
+      return '0$value';
+    } else {
+      return '$value';
+    }
+  }
+String _toDateString(DateTime dateTime) {
+  return '${dateTime.year}/${dateTime.month}/${dateTime.day}';
+}
+String _toTimeString(DateTime dateTime) {
+    return '${_twoDigits(dateTime.hour)}:${_twoDigits(dateTime.minute)}';
+  }
+
--- notetakr/lib/MyNote.dart
+++ notetakr/lib/MyNote.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyNote extends StatelessWidget
-{
-  String note_name; 
+class MyNote extends StatelessWidget {
+  String note_name;
 
-  MyNote(String note_name)
-  {
-    this.note_name = note_name; 
+  MyNote(String note_name) {
+    this.note_name = note_name;
   }
 
   @override
@@ -19,49 +17,41 @@ class MyNote extends StatelessWidget
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.edit),
-            onPressed: () 
-            {
+            onPressed: () {
               _navigateToAddNotes(note_name);
-                          },
-                        )
-        
-                      ],
-                    ),
-          body: Column(
-            
-            children: <Widget>[
-              
-                new Expanded(
-                  
-                  flex: 1,
-                  child: SingleChildScrollView(
-                    child: Padding (
-                      padding:  const EdgeInsets.all(7),
-                      child: new Text(
-                      "1 Description that is too long in text format(Here Data is coming from API) jdlksaf j klkjjflkdsjfkddfdfsdfds " + 
-                "2 Description that is too long in text format(Here Data is coming from API) d fsdfdsfsdfd dfdsfdsf sdfdsfsd d " + 
-                "3 Description that is too long in text format(Here Data is coming from API)  adfsfdsfdfsdfdsf   dsf dfd fds fs" + 
-                "4 Description that is too long in text format(Here Data is coming from API) dsaf dsafdfdfsd dfdsfsda fdas dsad" + 
-                "5 Description that is too long in text format(Here Data is coming from API) dsfdsfd fdsfds fds fdsf dsfds fds " + 
-                "6 Description that is too long in text format(Here Data is coming from API) asdfsdfdsf fsdf sdfsdfdsf sd dfdsf" + 
-                "7 Description that is too long in text format(Here Data is coming from API) df dsfdsfdsfdsfds df dsfds fds fsd" + 
-                "8 Description that is too long in text format(Here Data is coming from API)" + 
-                "9 Description that is too long in text format(Here Data is coming from API)" + 
-                "10 Description that is too long in text format(Here Data is coming from API)", 
-                 style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                    ),
-                  ),
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          new Expanded(
+            flex: 1,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(7),
+                child: new Text(
+                  "1 Description that is too long in text format(Here Data is coming from API) jdlksaf j klkjjflkdsjfkddfdfsdfds " +
+                      "2 Description that is too long in text format(Here Data is coming from API) d fsdfdsfsdfd dfdsfdsf sdfdsfsd d " +
+                      "3 Description that is too long in text format(Here Data is coming from API)  adfsfdsfdfsdfdsf   dsf dfd fds fs" +
+                      "4 Description that is too long in text format(Here Data is coming from API) dsaf dsafdfdfsd dfdsfsda fdas dsad" +
+                      "5 Description that is too long in text format(Here Data is coming from API) dsfdsfd fdsfds fds fdsf dsfds fds " +
+                      "6 Description that is too long in text format(Here Data is coming from API) asdfsdfdsf fsdf sdfsdfdsf sd dfdsf" +
+                      "7 Description that is too long in text format(Here Data is coming from API) df dsfdsfdsfdsfds df dsfds fds fsd" +
+                      "8 Description that is too long in text format(Here Data is coming from API)" +
+                      "9 Description that is too long in text format(Here Data is coming from API)" +
+                      "10 Description that is too long in text format(Here Data is coming from API)",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
                 ),
-              
-            ],
+              ),
+            ),
           ),
-                  );
-                }
-              
-                void _navigateToAddNotes(String note_name) {
+        ],
+      ),
+    );
+  }
 
-                  //TODO; 
-                }
-  
+  void _navigateToAddNotes(String note_name) {
+    //TODO;
+  }
 }

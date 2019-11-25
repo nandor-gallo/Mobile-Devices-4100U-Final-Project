22a21ffcc7d7635b474535322deced64159595f2
Leo Zbona <leonard.zbona@ontariotechu.net>
2019-11-25 17:07:49 -0500

    Fixed Assignment Model Class

--- notetakr/lib/model/assignment_model.dart
+++ notetakr/lib/model/assignment_model.dart
@@ -5,23 +5,50 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'assignment.dart';
 
 class AssignmentModel {
-  void insertGrade(String id,String title, Timestamp dueDate) async {
-    await Firestore.instance.collection("Assignments")
-      .document(id)
-      .setData({
-        'title': title,
-        'due': dueDate
-      });
+  void insertGrade(String id, String title, Timestamp dueDate) async {
+    await Firestore.instance
+        .collection("Assignments")
+        .document(id)
+        .setData({'title': title, 'due': dueDate});
   }
 
   void completedAssignment(String id) {
-  try {
-    print(id);
-    Firestore.instance.collection("Assignments")
-        .document(id)
-        .delete();
-  } catch (e) {
-    print(e.toString());
+    try {
+      print(id);
+      Firestore.instance.collection("Assignments").document(id).delete();
+    } catch (e) {
+      print(e.toString());
+    }
   }
-}
+
+
+  // Local Functions for assignment storage
+  Future<void> insertAssignment(Assignment assignment) async {
+    var db = await DBUtils.init();
+    db.insert(
+      'assignments',
+      assignment.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> updateAssignment(Assignment assignment) async {
+    var db = await DBUtils.init();
+    db.update(
+      'courses', 
+      assignment.toMap(), 
+      where: 'id = ?',
+      whereArgs: [assignment.id],
+    );   
+  }
+
+  Future<List<Assignment>> getAllAssignments() async {
+    var db = await DBUtils.init();
+    final List<Map<String,dynamic>> maps = await db.query('assignments');
+    List<Assignment> assignments = [];
+    for (int i = 0; i < maps.length; i++) {
+      assignments.add(Assignment.fromMap(maps[i]));
+    }
+    return assignments;
+  }
 }
